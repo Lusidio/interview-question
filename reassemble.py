4d77b778c47360a9b3e00e2818ac47f0c652a71e
James Webb <jmwebb@stanford.edu>
2016-08-05 18:37:53 -0700

    Add files via upload

---
+++ reassemble.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3 -tt
+"""
+File: reassemble.py
+-------------------
+Assignment 2: Quest for the Holy Grail
+Course: CS 41
+Name: James Webb
+SUNet: jmwebb
+
+Reassembles verticle shreds of images using comparing the left and right
+most column of each shred to every other shred. Column similarity is
+determined by taking the average similarity across all pixels. Pixel
+similarity is determined using Euclidean distance, treating a pixel as
+a vector of its color channel values.
+"""
+import imageutils as iu
+
+if __name__ == '__main__':
+    pass
+
+
+
+
